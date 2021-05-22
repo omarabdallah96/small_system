75e5bd4bfd6a4cdff8cebd7d0daaeb12ddd7776d
omarabdallah96 <71341894+omarabdallah96@users.noreply.github.com>
2021-05-22 13:41:27 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect('localhost','eb2a_28422515','','system_db');
+$con = mysqli_connect('localhost','root','','system_db');
 mysqli_set_charset($con,"utf8");
 if(mysqli_connect_errno()){
 	echo 'فشل الاتصال بقاعدة البيانات';
